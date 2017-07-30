9eb68927a6f783cf17fb56534ff814d849bc4217
curran <curran.kelleher@gmail.com>
2017-07-30 18:56:19 +0530

    Add quotes to failure case

--- logSpeed.sh
+++ logSpeed.sh
@@ -2,5 +2,5 @@
 echo "{\
   \"SSID\": \"`/sbin/iwgetid -r`\",\
   \"date\": \"`date --iso-8601=minutes`\",\
-  \"speedtest\": `speedtest-cli --json` \
+  \"speedtest\": `speedtest-cli --json | sed -En "s/Cannot retrieve speedtest configuration/\\"Cannot retrieve speedtest configuration\\"/p"`\
 }" >> speedLog.txt
