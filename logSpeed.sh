8d3e18509e093a5e87fc81bc9cd7e4706124ecb6
curran <curran.kelleher@gmail.com>
2017-07-30 19:05:13 +0530

    Fix sed issue

--- logSpeed.sh
+++ logSpeed.sh
@@ -2,5 +2,5 @@
 echo "{\
   \"SSID\": \"`/sbin/iwgetid -r`\",\
   \"date\": \"`date --iso-8601=minutes`\",\
-  \"speedtest\": `speedtest-cli --json | sed -En "s/Cannot retrieve speedtest configuration/\\"Cannot retrieve speedtest configuration\\"/p"`\
+  \"speedtest\": `speedtest-cli --json | sed "s/Cannot retrieve speedtest configuration/\\"Cannot retrieve speedtest configuration\\"/g"`\
 }" >> speedLog.txt
