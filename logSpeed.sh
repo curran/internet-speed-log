ef740809fdfa4c8eb598b69bd350fb6422ac72d8
curran <curran.kelleher@gmail.com>
2017-07-30 19:06:07 +0530

    Use .ndjson ext

--- logSpeed.sh
+++ logSpeed.sh
@@ -3,4 +3,4 @@ echo "{\
   \"SSID\": \"`/sbin/iwgetid -r`\",\
   \"date\": \"`date --iso-8601=minutes`\",\
   \"speedtest\": `speedtest-cli --json | sed "s/Cannot retrieve speedtest configuration/\\"Cannot retrieve speedtest configuration\\"/g"`\
-}" >> speedLog.txt
+}" >> speedLog.ndjson
